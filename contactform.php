6d3844a153b7c15c53c00beb69f5d66ea07bb1d8
Francisco Chiocconi <franchiocconi@MacBook-Pro.local>
2021-04-03 14:48:50 -0700

    carousel and contactus

--- contactform.php
+++ contactform.php
@@ -4,11 +4,11 @@
     $message = $_POST['message'];
 
     $email_from = "franciscochiocconi@gmail.com";
-    $email_subject = "Jorge and Roberto Contracting";
+    $email_subject = "J&R Construction";
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                         "User Message: $message.\n";
-    $to = "chiocconifrancisco@gmail.com";
+    $to = "franciscochiocconi@gmail.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to, $email_subject. $email_body, $headers);
